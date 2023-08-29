732fc8ca0c04d48ffa61edaf4470fb3052258d4a
Hong Kwang <74335429+kwang0149@users.noreply.github.com>
2023-08-29 08:27:03 +0700

    extract comment data from youtube

--- main.py
+++ main.py
@@ -1,24 +1,56 @@
-import tweepy
-import pandas as pd
+labels = ['Neutral Sentiment','Positive Sentiment','Negative Sentiment','Insulting the Government or Public Aegncy','Insulting or Defaming Others','Threatening Others','Alluding to the Tribe, Religion, Race, and intergroup']
+def predict(result,labels):
+  indices = tf.math.top_k(result, k=2).indices.numpy()[0]
+  # 'indices' now contains the indices of the top two maximum values
+  index1 = indices[0]
+  index2 = indices[1]
+  index3 = index1
+  if index1==2 and index2 == 1:
+    index3 = index2
+  result = []
+  result.append(labels[index1])
+  result.append(labels[index2])
+  result.append(labels[index3])
+  return result
 
-consumer_key = "b0utUzzGonPu4xxZGNRflJeA7"
-consumer_secret = "iXizRoP3nEnfa2MS8B19LbiADGscD52cxGwKsy0khEaKCIXzSG"
-access_key = "1084332182519201792-K1v36gFCgk6rOgjWvvkNtHaDz0g9fm"
-access_key_secret = "iP6YJRRPu4PDiHCGy5iyNYjXLdpMXHRY5XcuKLl2pIyxy"
-auth = tweepy.OAuthHandler(consumer_key, consumer_secret, access_key, access_key_secret)
-# auth.set_access_token(access_token, access_token_secret)
-api = tweepy.API(auth, wait_on_rate_limit=True)
-search_query = "'jokowi' -filter:retweets AND -filter:replies AND -filter:links AND -filter:images AND -filter:videos"
-no_of_tweets = 100
 
-try :
-    tweets = api.search_tweets(q=search_query, count=no_of_tweets, tweet_mode='extended')
-    attributes_container = [[tweet.user.name, tweet.created_at,tweet.favorite_count, tweet.source,tweet.full_text] for tweet in tweets]
-    columns = ["User","Data Created","Number of Likes","Source of Tweet","Tweet"]
 
-    tweets_df = pd.DataFrame(attributes_container, columns=columns)
 
-except BaseException as e:
-    print('failed on_status,', str(e))
+def comment_data(youtube_link):
+    api_service_name = "youtube"
+    api_version = "v3"
+    DEVELOPER_KEY = "AIzaSyAKCGnFw_ZbCwAbUgrfEJvwZJ-EZ1NgZxI"
 
-# tweets_df.to_csv("tweets.csv", index=False)
+    youtube = googleapiclient.discovery.build(
+        api_service_name, api_version, developerKey=DEVELOPER_KEY)
+
+    request = youtube.commentThreads().list(
+        part="snippet",
+        videoId= extract_video_id(youtube_link),
+        maxResults=1000
+    )
+    response = request.execute()
+    comments = []
+    for item in response['items']:
+        comment = item['snippet']['topLevelComment']['snippet']
+        comments.append([
+            comment['authorDisplayName'],
+            comment['publishedAt'],
+            comment['updatedAt'],
+            comment['likeCount'],
+            comment['textDisplay']
+        ])
+    df=pd.DataFrame(comments,columns=['author','published_at','updated_at','like_count','comment'])
+    first_choice_sentiments = []
+    second_choice_sentiments = []
+    final_sentiments = []
+    for comment in df['comment']:
+      result = model.predict([comment])
+      sentiment = predict(result,labels)
+      first_choice_sentiments.append(sentiment[0])
+      second_choice_sentiments.append(sentiment[1])
+      final_sentiments.append(sentiment[2])
+    df['first_choice_sentiment'] = first_choice_sentiments
+    df['second_choice_sentiment'] = second_choice_sentiments
+    df['final_sentiment'] = final_sentiments
+    return df
